45c7b7ba1e2e9236007905467c8998880514a4fa
Martin Honnen <martin.honnen@gmx.de>
2022-10-03 11:44:08 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Saxon.Api;
+using System.Xml;
+
+var processor = new Processor();
+
+var docBuilder = processor.NewDocumentBuilder();
+
+docBuilder.LineNumbering = true;
+
+docBuilder.BaseUri = new Uri("urn:from-string");
+
+const string mixedContent = @"<text>This is <b id=""b1"" style=""font-weight: bold;"">mixed</b> content: <!-- test -->Can XPath tools select a text node?</text>";
+
+var xdmDoc = docBuilder.Build(new StringReader(mixedContent));
+
+foreach (XdmNode node in processor.NewXPathCompiler().Evaluate("//*[@*]/(., @*)", xdmDoc))
+{
+    Console.WriteLine($"Node {node}; line: {node.LineNumber}; column: {node.ColumnNumber}");
+}
+
+using (var xr = XmlReader.Create(new StringReader(mixedContent)))
+{
+    while (xr.Read())
+    {
+        if (xr.HasAttributes)
+        {
+            Console.WriteLine($"Node {xr.NodeType}; Value: {xr.Value}; Line: {((IXmlLineInfo)xr).LineNumber}; Column: {((IXmlLineInfo)xr).LinePosition}");
+            xr.MoveToFirstAttribute();
+            do
+            {
+                Console.WriteLine($"Node {xr.NodeType}; Value: {xr.Value}; Line: {((IXmlLineInfo)xr).LineNumber}; Column: {((IXmlLineInfo)xr).LinePosition}");
+            }
+            while (xr.MoveToNextAttribute());
+        }
+    }
+}
+
+using (var xr = XmlReader.Create(new StringReader(mixedContent)))
+{
+    xdmDoc = docBuilder.Build(xr);
+
+    foreach (XdmNode node in processor.NewXPathCompiler().Evaluate("//@*", xdmDoc))
+    {
+        Console.WriteLine($"Node {node}; line: {node.LineNumber}; column: {node.ColumnNumber}");
+    }
+}
+
+
